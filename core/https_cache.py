4bec1a911dee2eb61de7c91383743652f4a3a82d
new493370 <new493370@gmail.com>
2026-06-19 11:52:27 +0330

    Create https_cache.py

---
+++ core/https_cache.py
@@ -0,0 +1,15 @@
+import time
+
+class HTTPSCache:
+    def __init__(self):
+        self.store = {}
+        self.expiry = {}
+
+    def get(self, key):
+        if key in self.expiry and time.time() < self.expiry[key]:
+            return self.store.get(key)
+        return None
+
+    def set(self, key, value, ttl=300):
+        self.store[key] = value
+        self.expiry[key] = time.time() + ttl
